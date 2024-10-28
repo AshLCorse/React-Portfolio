795511e466678b5d33e37690a7798e3bfdfd4d91
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-10-28 13:19:22 -0600

    merge conf

--- src/utils/projects.const.jsx
+++ src/utils/projects.const.jsx
@@ -15,3 +15,74 @@ const project = {
   liveLink: "",
   screenshot: 
 };
+
+// Add projects here
+
+projects.push({
+  title: "Project 1",
+  description: {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, nisl id consectetur pellentesque, velit sapien placerat velit, non consectetur neque enim ac lectus. Duis vel malesuada turpis. Sed tincidunt libero vel lectus facilisis, vel suscipit metus vulputate. Nulla facilisi.",
+    date: "2022-01-01",
+  },
+  dependencies: ["React", "Bootstrap", "Tailwind CSS"],
+  technologies: ["JavaScript", "React", "Bootstrap", "Tailwind CSS"],
+  githubLink: "https://github.com/username/project1",
+  liveLink: "https://project1.com",
+  screenshot: Screenshots.project1,
+});
+
+projects.push({
+  title: "Project 2",
+  description: {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, nisl id consectetur pellentesque, velit sapien placerat velit, non consectetur neque enim ac lectus. Duis vel malesuada turpis. Sed tincidunt libero vel lectus facilisis, vel suscipit metus vulputate. Nulla facilisi.",
+    date: "2022-02-01",
+  },
+  dependencies: ["React", "Bootstrap", "Tailwind CSS"],
+  technologies: ["JavaScript", "React", "Bootstrap", "Tailwind CSS"],
+  githubLink: "https://github.com/username/project2",
+  liveLink: "https://project2.com",
+  screenshot: Screenshots.project2,
+});
+
+// Add more projects here 
+
+    // ...
+
+export default projects;
+
+// In your components where you want to display these projects, you can use something like this: 
+
+    // ...
+
+    {projects.map((project) => (
+      <ProjectCard key={project.title} project={project} />
+    ))} 
+    
+    // ... 
+    
+    // In your ProjectCard component, you can use the screenshot prop to display the image: 
+    
+    // ...
+    
+    <img src={project.screenshot} alt={project.title} />
+    
+    // ...
+
+// You can add screenshots of each project in the assets folder, and reference them by their filename in the projects array. Make sure to import the screenshots in the same file where you define the projects array. 
+
+    // ...
+
+    import screenshots from "./assets/project1.png";
+    
+    // ... 
+    
+    const projects = [];
+    
+    // ...
+    
+    projects.push({
+      // ...
+      screenshot: screenshots,
+    });
+    
+    // ... 
