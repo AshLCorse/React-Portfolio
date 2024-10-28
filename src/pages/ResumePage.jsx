50638eda09f2f9e514c8654b4df79bbbb833a994
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-10-28 12:56:03 -0600

    done

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,204 +1,21 @@
 // This is a static page "Resue=me" section
+import Resume from "../utils/images/Tech-Resume-9_10_24.png";
+
 export default function ResumePage() {
   return (
-    <div className="container pt-4">
-      <section className="features-icons bg-light text-center m-4">
-        <div className="container">
-          <div className="row p-2">
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-window m-auto text-primary" />
-                </div>
-                <h3>Ashleigh Corse Resume</h3>
-                <p className="lead mb-0">
-                  (720) 800-1560 | ashleigh.corse@gmail.com Address: 5967 S
-                  Gallup St, Littleton CO, 80120
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-layers m-auto text-primary" />
-                </div>
-                <h3>Experience</h3>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Clearesult, Denver - Energy Technician II</h3>
-                <p className="lead mb-0">May 2023- Present</p>
-              </div>
-              <p>
-                {" "}
-                Conduct installation of residential energy efficient upgrades
-                such as LED light bulbs and water aerators, record and report
-                customer and contractor metrics, customer service and
-                interfacing, troubleshoot technical problems and issues
-                regarding installation in accordance with client specifications,
-                adapt constantly to changing environments, properties, and
-                units, follow program guidelines and the Fair Housing Act laws
-                and regulations, represent company to customers and contractors
-                professionally, travel throughout the greater Denver area, team
-                collaboration and communication, and data entry.
-              </p>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>
-                  Empower/HMEA, Denver - Software Developer working interview
-                </h3>
-                <p className="lead mb-0">April 2023</p>
-              </div>
-              <p>
-                {" "}
-                Selected to attend HMEA alternate interview process with
-                Empower. Participated in Front End team project. Used GitHub and
-                Visual Studio Code to make an interactive dashboard component
-                with JavaScript and React architecture over 3 days. Presented
-                project with team to members of management at Empower and HMEA.
-              </p>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-layers m-auto text-primary" />
-                </div>
-                <h3>Education</h3>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>
-                  Denver University/edX, Denver - Full Stack Flex Coding
-                  Bootcamp
-                </h3>
-                <p className="lead mb-0">April 2024 - Oct 2024</p>
-              </div>
-              <p>
-                {" "}
-                In this bootcamp I learned HTML, CSS, JavaScript, Git, Command
-                Terminal, Web APIs, Third Party APIs, jQuery, Bootstrap, Day.js,
-                Server-Side APIs, Fetch API, Node.js, npm, Object Oriented
-                Programming, Test-driven development, Promises, Express.js, POST
-                requests and persistent data, modular routing, custom middleware
-                and deployment, SQL, PostgreSQL, databases, Big Data, Object
-                Relational Mapping, Authentication, Sequelize,
-                Model-View-Controller, Handlebars.js, Partials, Helpers and
-                Authentication, Computer Science for JavaScript, How JavaScript
-                executes, Functional Programming in JavaScript, Algorithms,
-                Recursion, NoSQL, MongoDB, Mongoose, Progressive Web
-                Applications, Webpage performance and Webpack, Workbox, Caching
-                and Service Workers, IndexedDB and Progressive Web Applications,
-                React, React hooks, Conditional rendering, React router, MERN,
-                GraphQL, JSON Web Tokens, State, and Redux. I did 17 weekly solo
-                Challenges, and 3 2-week team Projects. They are on my GitHub,
-                which is <a href="https://github.com/AshLCorse">AshLCorse</a>
-              </p>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-layers m-auto text-primary" />
-                </div>
-                <h3>Skills</h3>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <p>
-                <ul>
-                  <li>
-                    Adept at hands on learning, picks up new skills quickly and
-                    efficiently.
-                  </li>
-                  <li>Strong communication and teamwork skills.</li>
-                  <li>Strong analytical and problem-solving skills.</li>
-                  <li>
-                    Proficient in HTML, CSS, JavaScript, Git, Node.js, Object
-                    Oriented Programming, Jest, Test-driven development,
-                    Express.js, Object Relational Mapping, React.js, and
-                    GraphQL, as well as other modern web technologies.
-                  </li>
-                  <li>Types at 70 WPM</li>
-                  <li>
-                    Can work independently, collaborate effectively with team
-                    members, and communicate effectively with stakeholders and
-                    customers.
-                  </li>
-                  <li>UX/UI development and testing.</li>
-                  <li>
-                    Experience with various debugging tools, such as Chrome
-                    DevTools, Firefox Developer Tools, and Visual Studio Code.
-                  </li>
-                  <li>
-                    Experience with various web security tools, such as HTTPS.
-                  </li>
-                </ul>
-              </p>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-layers m-auto text-primary" />
-                </div>
-                <h3>References</h3>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Jay Mascarenas</h3>
-                <p className="lead mb-0">jmascarenas@instructors.2u.com</p>
-              </div>
-              <p>Instructor for DU edX bootcamp</p>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Jared Koppa</h3>
-                <p className="lead mb-0">
-                  jared.koppa@clearesult.com (303) 419-0009
-                </p>
-              </div>
-              <p>Crew Supervisor at Clearesult</p>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Russel Leete</h3>
-                <p className="lead mb-0">rleete@hmea.org</p>
-              </div>
-              <p> VP of Employment Services, N@W and TRACS</p>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Arielle West</h3>
-                <p className="lead mb-0">arielle@ariellewest.com</p>
-              </div>
-              <p>Programming Tutor, has a BS in Software Engineering</p>
-            </div>
-          </div>
-        </div>
-      </section>
+    <div>
+      <h2>Resume</h2>
+      <img src={Resume} alt="Ashleigh Corse's Resume" />
+      <ul>
+        <li>
+          <a href="https://github.com/AshLCorse">My Github Profile</a>
+        </li>
+        <li>
+          <a href="http://www.linkedin.com/in/ash-corse-2852a1126">
+            My LinkedIn Profile
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
